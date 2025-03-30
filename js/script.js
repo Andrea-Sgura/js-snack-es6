e93a092929643fa08d6a6baf24a31077e521c3f9
Andrea-Sgura <andreasgura96@gmail.com>
2025-03-31 00:39:42 +0200

    [UPD] Snack1: Creo l'array di oggetti

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// CREO L'ARRAY DI OGGETTI
+const bikes = [
+    {
+        nome: "Bianchi",
+        peso: 7.5,
+    },
+    {
+        nome: "Trek",
+        peso: 8.2,
+    },
+    {
+        nome: "Giant",
+        peso: 7.8,
+    },
+    {
+        nome: "Cannondale",
+        peso: 6.9,
+    },
+    {
+        nome: "Specialized",
+        peso: 7.2,
+    }
+]
